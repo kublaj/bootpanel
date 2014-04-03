6f7b29291f6458130f1bc90ef6f3af8f230eaacb
nailshopdev <danielakos@blan.hu>
2014-04-03 23:35:07 +0200

    fixed jslint errors
    Bootpanel (0.4)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,11 @@
 /*app.js*/
-"use strict" // _jsapi;
+
+var $;
+var console;
 
 function panel() {
+    
+    "use strict";
 
     if ($("#panel").hasClass("cbp-spmenu-open")) {
 
@@ -15,6 +19,8 @@ function panel() {
 }
 
 $("#toggle").click(function () {
+    
+    "use strict";
 
     $(this).data('clicked', true);
     console.log('panel hide');
@@ -24,6 +30,8 @@ $("#toggle").click(function () {
 });
 
 $('body').on("swipeleft", function (event) {
+    
+    "use strict";
 
     console.log('swipe left');
     $("#panel").removeClass('cbp-spmenu-open');
@@ -31,9 +39,10 @@ $('body').on("swipeleft", function (event) {
 });
 
 $('body').on("swiperight", function (event) {
+    
+    "use strict";
 
     console.log('swipe right');
     $("#panel").addClass('cbp-spmenu-open');
 
 });
-
